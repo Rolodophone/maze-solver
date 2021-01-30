2f988380c76db488a9ff65809328f97f73b70393
Sergi Lange-Soler <sergi.lange123@gmail.com>
2021-01-30 13:02:54 +0000

    prefer going to the bottom right

--- src/main/kotlin/Square.kt
+++ src/main/kotlin/Square.kt
@@ -1,6 +1,6 @@
 class Square(val x: Int, val y: Int, var left: Square?, var up: Square?, var right: Square?, var down: Square?): Iterable<Square> {
     override fun iterator(): Iterator<Square> {
-        val tempList = mutableListOf(left, up, right, down)
+        val tempList = mutableListOf(right, down, up, left) //prefer going to to the bottom right
         tempList.removeAll { it == null }
 
         val list = tempList.map { it!! }
